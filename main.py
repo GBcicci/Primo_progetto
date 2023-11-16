0a874e13c6549b7f7282773ea7e5844f74327e34
Giuseppe Base <g.base@itsrizzoli.it>
2023-11-16 17:08:17 +0100

    Aggiunta funzione statistica

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas as pd
-#commit 1 funzione main--> apre il csv con pd
-#commit 2 richiama main--> funzione statistica per calcolare qualcosa
-def leggi(path:str)->pd.DataFrame:
+
+
+# commit 1 funzione main--> apre il csv con pd
+# commit 2 richiama main--> funzione statistica per calcolare qualcosa
+
+def leggi(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
 
+def statistica(df: pd.DataFrame) -> str:
+    # media ritardi sull aeroporto di Birminghan nel 2020
+    df_filtered = df[(df['airport'] == 'BHM') & (df['year'] == 2020)]
+    avg_dealy = df_filtered['arr_delay'].mean()
+    print(f"media ritardi:{avg_dealy}")
+
+
 if __name__ == '__main__':
-    path=r"C:\Users\GiuseppeBase\PycharmProjects\MachineLearning\Data_lake\Airline Delay.csv"
-    df=leggi(path)
-    print(df)
+    path = r"C:\Users\GiuseppeBase\PycharmProjects\MachineLearning\Data_lake\Airline Delay.csv"
+    statistica(leggi(path))

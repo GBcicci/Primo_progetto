79f0cf0d380a5aa9a2360ba44863e16dffec4816
Greta Savio <g.savio@itsrizzoli.it>
2023-11-16 17:16:24 +0100

    Statistiche descrittive riassuntive

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import pandas as pd
 #commit 1 funzione main--> apre il csv con pd
 #commit 2 richiama main--> funzione statistica per calcolare qualcosa
 
+def statistica_canciani_savio(df):
+    print(df.describe())
+
 if __name__ == '__main__':
     path = r"Airline Delay.csv"
     df = pd.read_csv(path, sep=',', header=0)
     print(df.head)
+
+    statistica_canciani_savio(df)
+
+
 
